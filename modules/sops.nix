9abac606809518d7cecc571f9006f7ff7b101d33
Collin Arnett <collin@arnett.it>
2022-01-13 02:09:46 -0500

    added dot for relative path

--- modules/sops.nix
+++ modules/sops.nix
@@ -1,5 +1,5 @@
 {
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = ../secrets/secrets.yaml;
   sops.age.keyFile = "/home/collin/.config/sops/age/keys.txt";
   sops.secrets.awscli2-config = { };
   sops.secrets.awscli2-credentials = { };
