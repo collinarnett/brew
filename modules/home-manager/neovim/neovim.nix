06fa167014ac861bc490842ad3a364f76f5dd1f1
Collin Arnett <collin@arnett.it>
2023-05-18 15:02:28 -0400

    Removed bloat from neovim config

--- modules/home-manager/neovim/neovim.nix
+++ modules/home-manager/neovim/neovim.nix
@@ -225,51 +225,6 @@
         plugin = nvim-lspconfig;
         type = "lua";
         config = ''
-            local opts = { noremap=true, silent=true }
-            vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-            vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
-
-            -- Use an on_attach function to only map the following keys
-            -- after the language server attaches to the current buffer
-            local on_attach = function(client, bufnr)
-              -- Enable completion triggered by <c-x><c-o>
-              vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-              vim.api.nvim_create_autocmd("CursorHold", {
-                buffer = bufnr,
-                callback = function()
-                  local opts = {
-                    focusable = false,
-                    close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-                    border = 'rounded',
-                    source = 'always',
-                    prefix = ' ',
-                    scope = 'cursor',
-                  }
-                  vim.diagnostic.open_float(nil, opts)
-                end
-              })
-
-              -- Mappings.
-              -- See `:help vim.lsp.*` for documentation on any of the below functions
-              local bufopts = { noremap=true, silent=true, buffer=bufnr }
-              vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-              vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-              vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-              vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-              vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-              vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-              vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-              vim.keymap.set('n', '<space>wl', function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-              end, bufopts)
-              vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-              vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-              vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-              vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-              vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-            end
             require('lspconfig').nil_ls.setup({
               cmd = { "${pkgs.nil}/bin/nil" }
             })
@@ -281,7 +236,7 @@
               cmd = { "${pkgs.java-language-server}/bin/java-language-server" },
               settings = {
                 java = {
-                   classPath = {"carnett3/483libs.jar"}
+                  classPath = {"carnett3/483libs.jar"}
                 }
               }
             })
