0c598608fd0a00247159f0738cb15a0b689f49ff
Collin Arnett <collin@arnett.it>
2023-05-18 22:44:48 -0400

    * Added moody for emacs menu bar. * Removed emacs ui elements. * Added magit to emacs. * Selected correct font for emacs.

--- modules/emacs/emacs.el
+++ modules/emacs/emacs.el
@@ -1,3 +1,19 @@
+;; disable gui elements
+(menu-bar-mode -1) 
+(scroll-bar-mode -1) 
+(tool-bar-mode -1)
+
+(use-package moody
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (moody-replace-eldoc-minibuffer-message-function))
+
+;; font 
+(add-to-list 'default-frame-alist
+             '(font . "Fira Code"))
+
 ;; vim keybinds
 (use-package evil
   :demand t
@@ -14,3 +30,5 @@
 (use-package dracula-theme
   :config
   (load-theme 'dracula t))
+
+(use-package magit)
