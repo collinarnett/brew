618027b24533f9e5b283938f5c5a9f832f3e4104
Collin Arnett <collin@arnett.it>
2023-07-28 17:17:04 -0400

    Added discord teams and slack

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -9,12 +9,18 @@
   home.homeDirectory = "/Users/collin/";
 
   home.packages = with pkgs; [
+    discord
     fira-code
     nur.repos.toonn.apps.firefox
     git
     nerdfonts
     pfetch
     noto-fonts-emoji
+    teams
+    slack
+    # apps I would like but are not availible for aarch64-darwin
+    # firefox
+    # signal-desktop
   ];
 
   home.stateVersion = "21.11";
