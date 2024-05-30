1d26bd87eddf4ef5d59bbb6585d9c045c620c7d4
Collin Arnett <collin@arnett.it>
2024-05-30 17:40:23 -0400

    Added local docker registry for easier development

--- modules/docker-registry.nix
+++ modules/docker-registry.nix
@@ -2,4 +2,5 @@
   services.dockerRegistry.enable = true;
   services.dockerRegistry.garbageCollectDates = "monthly";
   services.dockerRegistry.enableGarbageCollect = true;
+  services.dockerRegistry.listenAddress = "0.0.0.0";
 }
