0415d254abd7b5e9e84c657a1f6d1b920637dab4
Collin Arnett <collin@arnett.it>
2024-09-28 15:59:14 -0400

    Created overlay to fix whipper installation, fixed deprecated nix version declaration

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
               ./hosts/${host}/configuration.nix
               {
                 nix = {
-                  package = pkgs.nixUnstable;
+                  package = pkgs.nixVersions.latest;
                   registry.pkgs.flake = nixpkgs;
                   nixPath = ["nixpkgs=${nixpkgs}"];
                   settings = {
@@ -71,7 +71,25 @@
                     ];
                   };
                 };
-                nixpkgs.overlays = [emacs-overlay.overlay (import ./pkgs/all-packages.nix)];
+                nixpkgs.overlays = [
+                  (final: prev: {
+                    python312 = prev.python312.override {
+                      packageOverrides = finalPkgs: prevPkgs: {
+                        nose = prevPkgs.nose.overrideAttrs {
+                          patches = [
+                            (final.fetchpatch2 {
+                              url = "https://github.com/NixOS/nixpkgs/raw/599e471d78801f95ccd2c424a37e76ce177e50b9/pkgs/development/python-modules/nose/0001-nose-python-3.12-fixes.patch";
+                              hash = "sha256-aePOvO5+TJL4JzXywc7rEiYRzfdObSI9fg9Cfrp+e2o=";
+                            })
+                          ];
+                        };
+                      };
+                    };
+                    python312Packages = final.python312.pkgs;
+                  })
+                  emacs-overlay.overlay
+                  (import ./pkgs/all-packages.nix)
+                ];
               }
 
               sops-nix.nixosModules.sops
