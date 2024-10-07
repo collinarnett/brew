9887aec88a8566ef6d7ddd693de3e707be3c86b4
Collin Arnett <collin@arnett.it>
2024-10-06 21:31:09 -0400

    Added important directories to persist

--- hosts/azathoth/impermanence.nix
+++ hosts/azathoth/impermanence.nix
@@ -1,5 +1,12 @@
 {
   # TODO: Figure out what needs to be added here to get a MVP working
+  environment.persistence."/persist" = {
+    directories = [
+      "/var/log"
+      "/var/lib/nixos"
+      "/var/lib/systemd/coredump"
+    ];
+  };
   environment.persistence."/persist/save" = {
     users.collin = {
       directories = [
