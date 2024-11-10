897e26bd4c9eefbcce375982860e588e7ba3c917
Collin Arnett <collin@arnett.it>
2024-11-10 12:24:43 -0500

    Removed deprecated settings

--- modules/home-manager/waybar/waybar.nix
+++ modules/home-manager/waybar/waybar.nix
@@ -6,19 +6,17 @@
         modules-left = ["cpu" "pulseaudio" "disk" "memory" "mpd" "network"];
         modules-center = ["sway/workspaces"];
         modules-right = ["clock"];
-        modules = {
-          "sway/workspaces" = {
-            format = "{icon}";
-            format-icons = {default = "";};
-          };
-          "pulseaudio" = {format = " {volume}%";};
-          "mpd" = {format = " {title}";};
-          "clock" = {format = " {:%A, %h %d %I:%M %p}";};
-          "cpu" = {format = " {usage}%";};
-          "disk" = {format = " {percentage_used}%";};
-          "memory" = {format = " {used:0.1f}G";};
-          "network" = {format = "{ifname}: {bandwidthDownBits} | {bandwidthUpBits}";};
+        "sway/workspaces" = {
+          format = "{icon}";
+          format-icons = {default = "";};
         };
+        "pulseaudio" = {format = " {volume}%";};
+        "mpd" = {format = " {title}";};
+        "clock" = {format = " {:%A, %h %d %I:%M %p}";};
+        "cpu" = {format = " {usage}%";};
+        "disk" = {format = " {percentage_used}%";};
+        "memory" = {format = " {used:0.1f}G";};
+        "network" = {format = "{ifname}: {bandwidthDownBits} | {bandwidthUpBits}";};
       };
     };
     style = ./style.css;
