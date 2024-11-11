ee25e9cafbec49878ceec11f3b20e14ea75908f7
Collin Arnett <collin@arnett.it>
2024-11-11 11:19:39 -0500

    Moved audio specific packages into pipewire config

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,8 +1,12 @@
-{
+{pkgs, ...}: {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
     socketActivation = true;
   };
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+    helvum
+  ];
 }
