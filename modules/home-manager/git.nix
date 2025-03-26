8486c5c527809c733443ad15e60856c30ea6b258
Collin Arnett <collin@arnett.it>
2025-03-26 10:27:05 -0400

    Changed git configuration to make magit more performant with nixpkgs

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -4,7 +4,19 @@
     userEmail = "collin@arnett.it";
     userName = "Collin Arnett";
     difftastic.enable = false;
-    extraConfig = {commit = {verbose = true;};};
+    extraConfig = {
+      rebase = {
+        autostash = true;
+      };
+      commit = {verbose = true;};
+      core = {
+        fsmonitor = true;
+        untrackedcache = true;
+      };
+      fetch = {
+        writeCommitGraph = true;
+      };
+    };
     signing = {
       key = "A85650D42EB741D9";
       signByDefault = true;
