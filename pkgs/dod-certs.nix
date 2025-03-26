fc0551d69c83de6439af19e378eb578411192f7f
Collin Arnett <collin@arnett.it>
2025-03-26 10:24:57 -0400

    Updated DoD Certs

--- pkgs/dod-certs.nix
+++ pkgs/dod-certs.nix
@@ -6,17 +6,18 @@
 }:
 stdenv.mkDerivation {
   pname = "dod-certs";
-  version = "5.13.0";
+  version = "5.14.0";
 
   src = fetchzip {
     url = "https://dl.dod.cyber.mil/wp-content/uploads/pki-pke/zip/unclass-certificates_pkcs7_DoD.zip";
-    sha256 = "sha256-QgRw3UPTTXNadkpo5FonQ6hTsQVnk6l/2n2hnLOaL1E=";
+    sha256 = "sha256-HhbGyHgwV8bbZutDqhHriso3y84XxumtuED9BHO0XEk=";
   };
 
   nativeBuildInputs = [unzip openssl];
 
   buildPhase = ''
-    openssl pkcs7 -in certificates_pkcs7_v5_13_dod_der.p7b -inform der -print_certs -out dod_CAs.pem
+    ls
+    openssl pkcs7 -in Certificates_PKCS7_v5_14_DoD.der.p7b -inform der -print_certs -out dod_CAs.pem
   '';
 
   installPhase = ''
