ff903565b9613e1d11c413ad40abee2d50b998d4
Collin Arnett <collin@arnett.it>
2026-02-11 16:36:03 -0500

    fix: allow pcscd access from remote sessions (SSH/waypipe)

--- modules/cac.nix
+++ modules/cac.nix
@@ -38,6 +38,18 @@ in
 
     services.pcscd.enable = true;
 
+    # Allow pcscd access from remote sessions (SSH/waypipe)
+    # Without this, polkit rejects smart card access for non-local sessions
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if ((action.id == "org.debian.pcsc-lite.access_pcsc" ||
+             action.id == "org.debian.pcsc-lite.access_card") &&
+            subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+
     security.pam.p11.enable = true;
     security.pki.certificateFiles = [
       "${pkgs.dod-certs}/dod-certs.pem"
