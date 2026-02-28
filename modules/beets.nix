070e5d9904e4364a1f3da47d1c887c3ba1d30bd9
Collin Arnett <collin@arnett.it>
2026-02-28 14:30:07 -0500

    fix: overlay beets to remove sphinx deps (nixpkgs#493540)

--- modules/beets.nix
+++ modules/beets.nix
@@ -6,6 +6,17 @@ in
 {
   options.brew.beets.enable = lib.mkEnableOption "beets";
   config = lib.mkIf cfg.enable {
+    # TODO: Remove overlay once nixpkgs#493540 is resolved (autodocsumm vs sphinx 9)
+    nixpkgs.overlays = [
+      (final: prev: {
+        beets = prev.beets.overridePythonAttrs (old: {
+          outputs = [ "out" ];
+          nativeBuildInputs = builtins.filter (
+            dep: !(lib.hasInfix "sphinx" (lib.toLower (dep.pname or dep.name or "")))
+          ) (old.nativeBuildInputs or [ ]);
+        });
+      })
+    ];
     home-manager.users.${user} = {
       programs.beets = {
         enable = true;
